249f81c79a4ee230009dca8caab86d4177e332cf
Tom Moseley <tommoseley@outlook.com>
2025-12-17 18:52:00 -0500

    pos rework

--- app/api/models/__init__.py
+++ app/api/models/__init__.py
@@ -2,11 +2,8 @@
 Models for The Combine.
 """
 from app.api.models.project import Project
-from app.api.models.artifact import Artifact
-from app.api.models.artifact_version import ArtifactVersion
 from app.api.models.workflow import Workflow
 from app.api.models.file import File
-from app.api.models.breadcrumb_file import BreadcrumbFile
 from app.api.models.role_prompt import RolePrompt
 from app.api.models.role import Role
 from app.api.models.role_task import RoleTask
@@ -15,11 +12,8 @@ from app.api.models.document import Document
 from app.api.models.document_relation import DocumentRelation, RelationType
 __all__ = [
     'Project',
-    'Artifact',
-    'ArtifactVersion',
     'Workflow',
     'File',
-    'BreadcrumbFile',
     'RolePrompt',
     'Role',
     'RoleTask',
