0018cf5886c660b70715abf3e3c6e7b0a17e5902
Tom Moseley <tommoseley@outlook.com>
2025-12-21 11:57:38 -0500

    Fix Workflow

--- app/api/models/__init__.py
+++ app/api/models/__init__.py
@@ -2,7 +2,6 @@
 Models for The Combine.
 """
 from app.api.models.project import Project
-from app.api.models.workflow import Workflow
 from app.api.models.file import File
 from app.api.models.role_prompt import RolePrompt
 from app.api.models.role import Role
@@ -12,7 +11,6 @@ from app.api.models.document import Document
 from app.api.models.document_relation import DocumentRelation, RelationType
 __all__ = [
     'Project',
-    'Workflow',
     'File',
     'RolePrompt',
     'Role',
